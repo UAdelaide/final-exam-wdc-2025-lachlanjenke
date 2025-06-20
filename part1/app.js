6ecb18e0467b3cc6d99bafd69d963b269d84dd81
Lachlan Jenke <a1924627@adelaide.edu.au>
2025-06-20 09:39:18 +0000

    [automatic commit] 2025-06-20 09:39:18 UTC 180.150.112.203

--- part1/app.js
+++ part1/app.js
@@ -14,7 +14,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 (async () => {
-    try()
+    try {
+
+    } catch ()
 })
 
 app.use(express.static(path.join(__dirname, 'public')));
