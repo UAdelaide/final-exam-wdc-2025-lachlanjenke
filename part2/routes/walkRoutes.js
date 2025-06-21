211f8dc21979111d87dfe9ce5f6521687b7c26bd
Lachlan Jenke <a1924627@adelaide.edu.au>
2025-06-21 06:11:36 +0000

    [automatic commit] 2025-06-21 06:11:35 UTC 180.150.112.203

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -22,14 +22,14 @@ router.get('/', async (req, res) => {
 // Get all walk requests (for owners to view)
 // GET all walk requests (for walkers to view)
 router.get('/ownerWalks', async (req, res) => {
-  const 
+  const ownerId = req.session.user.user_id;
   try {
     const [rows] = await db.query(`
       SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id = d.dog_id
       JOIN Users u ON d.owner_id = u.user_id
-      WHERE wr.status = 'open'
+      WHERE wr.status = 'open' AND 
     `);
     res.json(rows);
   } catch (error) {
