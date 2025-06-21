16ee3192b499b710ffa991b469c529b96386665b
Lachlan Jenke <a1924627@adelaide.edu.au>
2025-06-21 06:40:56 +0000

    [automatic commit] 2025-06-21 06:40:56 UTC ;; communications error to 208.67.222.222#53: timed out 180.150.112.203

--- part2/app.js
+++ part2/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 require('dotenv').config();
+const db = require('./models/db');
 
 const app = express();
 
